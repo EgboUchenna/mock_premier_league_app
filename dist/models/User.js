9eb7a9ed31dde5cfb81a1d936877fd96735a4e94
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-09-26 15:46:41 +0100

    Add user token.

--- dist/models/User.js
+++ dist/models/User.js
@@ -1,12 +1,26 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var ts_mongoose_1 = require("ts-mongoose");
-var UserSchema = ts_mongoose_1.createSchema({
-    name: ts_mongoose_1.Type.string(),
-    email: ts_mongoose_1.Type.string({ unque: true }),
-    password: ts_mongoose_1.Type.string(),
-    isAdmin: ts_mongoose_1.Type.optionalBoolean(),
-});
-UserSchema.methods.getAuthToken = function () { };
+var jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+var config_1 = __importDefault(require("config"));
+var UserSchema = ts_mongoose_1.createSchema(__assign({ name: ts_mongoose_1.Type.string(), email: ts_mongoose_1.Type.string({ unque: true }), password: ts_mongoose_1.Type.string(), isAdmin: ts_mongoose_1.Type.optionalBoolean() }, {}));
+UserSchema.methods.getAuthToken = function () {
+    var token = jsonwebtoken_1.default.sign({ _id: this._id, isAdmin: this.isAdmin }, config_1.default.get('jwtPrivateKey'));
+    return token;
+};
 exports.User = ts_mongoose_1.typedModel('User', UserSchema);
 //# sourceMappingURL=User.js.map
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,23 @@
 import { createSchema, Type, typedModel } from 'ts-mongoose';
+import jwt from 'jsonwebtoken';
+const { key } = require('../config/keys');
 
 const UserSchema = createSchema({
   name: Type.string(),
   email: Type.string({ unque: true }),
   password: Type.string(),
   isAdmin: Type.optionalBoolean(),
+  ...({} as {
+    getAuthToken: () => string;
+  }),
 });
 
-UserSchema.methods.getAuthToken = () => { };
+UserSchema.methods.getAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    key,
+  );
+  return token;
+};
+
 export const User = typedModel('User', UserSchema);
