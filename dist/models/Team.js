22c265290656e7c7f631fe55e5bf5fade86716e0
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-09-26 21:35:39 +0100

    Created Team model.

---
+++ dist/models/Team.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ts_mongoose_1 = require("ts-mongoose");
+var TeamSchema = ts_mongoose_1.createSchema({
+    name: ts_mongoose_1.Type.string(),
+    nick_name: ts_mongoose_1.Type.string(),
+    website: ts_mongoose_1.Type.string(),
+    coach: ts_mongoose_1.Type.string(),
+    founded: ts_mongoose_1.Type.number(),
+    stadium_name: ts_mongoose_1.Type.string(),
+    stadium_capacity: ts_mongoose_1.Type.string(),
+});
+exports.Team = ts_mongoose_1.typedModel('Team', TeamSchema);
+//# sourceMappingURL=Team.js.map
---
+++ src/models/Team.ts
@@ -0,0 +1,13 @@
+import { createSchema, Type, typedModel } from 'ts-mongoose';
+
+const TeamSchema = createSchema({
+  name: Type.string(),
+  nick_name: Type.string(),
+  website: Type.string(),
+  coach: Type.string(),
+  founded: Type.number(),
+  stadium_name: Type.string(),
+  stadium_capacity: Type.string(),
+});
+
+export const Team = typedModel('Team', TeamSchema);
