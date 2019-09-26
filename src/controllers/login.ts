b047ba4a0490a4117e645f685a925af57a8809fd
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-09-26 13:22:22 +0100

    Created login controller.

---
+++ src/controllers/login.ts
@@ -0,0 +1,17 @@
+import { User } from '../models/User';
+import bcrypt from 'bcrypt';
+import { Response, Request } from 'express';
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const checkUser = await User.findOne({ email });
+  if (!checkUser) return res.status(404).send({ message: 'Email does not exist.' });
+
+  const passwordMatch = await bcrypt.compare(password, checkUser.password);
+  if (!passwordMatch) {
+    return res.status(404).send({ message: 'Password is incorrect.' });
+  }
+
+  // Send a token in the request header
+  return res.status(200).send({ message: `welcome ${checkUser.name}` });
+};
