17ecb30577e6d19d5be799596af129ebfb62f389
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-09-26 13:51:23 +0100

    Add routes for user signup and login.

---
+++ src/routes/api/index.ts
@@ -0,0 +1,7 @@
+import user from './user';
+import { Router } from 'express';
+
+const router = Router();
+router.use('/users', user);
+
+export default router;
---
+++ src/routes/api/user.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { login } from '../../controllers/login';
+import { signup } from '../../controllers/signup';
+
+const router = Router();
+
+router
+  .post('/signup', signup)
+  .post('/login', login);
+
+export default router;
