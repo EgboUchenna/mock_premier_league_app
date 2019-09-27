fdded09f363767f26b48221d0e0d442f608b4d9d
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-09-27 02:36:17 +0100

    Expose team and fixture routes.

--- dist/routes/api/index.js
+++ dist/routes/api/index.js
@@ -4,11 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 // tslint:disable: import-name
+var fixture_1 = __importDefault(require("./fixture"));
 var user_1 = __importDefault(require("./user"));
 var team_1 = __importDefault(require("./team"));
 var express_1 = require("express");
 var router = express_1.Router();
-router.use('/users', user_1.default);
+router.use('/fixtures', fixture_1.default);
 router.use('/teams', team_1.default);
+router.use('/users', user_1.default);
 exports.default = router;
 //# sourceMappingURL=index.js.map
--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,11 +1,14 @@
 // tslint:disable: import-name
+import fixturesRoute from './fixture';
 import usersRoute from './user';
 import teamsRoute from './team';
 
 import { Router } from 'express';
 
 const router = Router();
-router.use('/users', usersRoute);
+
+router.use('/fixtures', fixturesRoute);
 router.use('/teams', teamsRoute);
+router.use('/users', usersRoute);
 
 export default router;
