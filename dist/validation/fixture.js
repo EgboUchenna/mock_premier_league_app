b301ebe8a761172fd2b86ba1e214264a181655fc
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-09-27 01:22:04 +0100

    Add fixture validation.

---
+++ dist/validation/fixture.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// tslint:disable: import-name
+var joi_1 = __importDefault(require("joi"));
+var joi_objectid_1 = __importDefault(require("joi-objectid"));
+var myJoiObjectId = joi_objectid_1.default(joi_1.default);
+exports.validateFixture = function (input) {
+    var schema = {
+        homeTeam: myJoiObjectId().required(),
+        awayTeam: myJoiObjectId().required(),
+        homeScore: joi_1.default.number(),
+        awayScore: joi_1.default.number(),
+        time: joi_1.default.number(),
+        stadium: joi_1.default.string(),
+        played: joi_1.default.boolean(),
+    };
+    return joi_1.default.validate(input, schema);
+};
+//# sourceMappingURL=fixture.js.map
---
+++ src/validation/fixture.ts
@@ -0,0 +1,18 @@
+// tslint:disable: import-name
+import Joi from 'joi';
+import JoiObjectId from 'joi-objectid';
+const myJoiObjectId = JoiObjectId(Joi);
+
+export const validateFixture = (input: object) => {
+  const schema = {
+    homeTeam: myJoiObjectId().required(),
+    awayTeam: myJoiObjectId().required(),
+    homeScore: Joi.number(),
+    awayScore: Joi.number(),
+    time: Joi.number(),
+    stadium: Joi.string(),
+    played: Joi.boolean(),
+  };
+
+  return Joi.validate(input, schema);
+};
