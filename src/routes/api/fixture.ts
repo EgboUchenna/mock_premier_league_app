adbaae67bc2ddfa448bef7e6e687f21dd9549182
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-09-27 04:19:23 +0100

    Add authentication for fixtures.

--- src/routes/api/fixture.ts
+++ src/routes/api/fixture.ts
@@ -1,19 +1,26 @@
 import { Router } from 'express';
+import auth from '../../middleware/auth';
+import admin from '../../middleware/admin';
+
 import {
   createFixtures,
   deleteFixture,
   getFixture,
   updateFixture,
   viewFixtures,
+  viewPendingMatches,
+  viewPlayedMatches,
 } from '../../controllers/fixture';
 
 const router = Router();
 
 router
-  .get('/', viewFixtures)
+  .get('/', auth, viewFixtures)
+  .get('/played', viewPlayedMatches)
+  .get('/pending', viewPendingMatches)
   .get('/one', getFixture)
-  .post('/', createFixtures)
-  .put('/:id', updateFixture)
-  .delete('/:id', deleteFixture);
+  .post('/', [auth, admin], createFixtures)
+  .put('/:id', [auth, admin], updateFixture)
+  .delete('/:id', [auth, admin], deleteFixture);
 
 export default router;
